c95f1dc0f3023035a4e1dbc6d629dfb1fc5f5f96
ankitbisen <bisenankit21@gmail.com>
2025-01-02 18:44:35 +0530

    Initial commit.

---
+++ project6/src/main/java/com/ankit/main/Project4.java
@@ -0,0 +1,15 @@
+package com.ankit.main;
+
+import com.ankit.beans.Vehicle;
+import com.ankit.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Project4 {
+    public static void main(String[] args) {
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+        Vehicle vehicle = context.getBean(Vehicle.class);
+        System.out.println("Vehicle name from Spring context is:"+vehicle.getName());
+        vehicle.printHello();
+
+    }
+}
