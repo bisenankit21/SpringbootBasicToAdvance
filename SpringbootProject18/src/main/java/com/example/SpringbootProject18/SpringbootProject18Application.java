1cee1a12d945485652d2097126ac0e76ba9966cc
ankitbisen <bisenankit21@gmail.com>
2025-01-23 18:04:14 +0530

    Initial Commit

---
+++ SpringbootProject18/src/main/java/com/example/SpringbootProject18/SpringbootProject18Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootProject18;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProject18Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProject18Application.class, args);
+	}
+
+}
---
+++ SpringbootProject18/src/test/java/com/example/SpringbootProject18/SpringbootProject18ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringbootProject18;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootProject18ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
