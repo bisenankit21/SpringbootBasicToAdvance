6da8d51af095f547267014c3dd6d33841610b311
ankitbisen <bisenankit21@gmail.com>
2025-01-23 22:41:28 +0530

    initial commit

--- SpringbootProject18/src/test/java/com/example/SpringbootProject18/EazySchoolApplicationTests.java
+++ SpringbootProject18/src/test/java/com/example/SpringbootProject18/EazySchoolApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class SpringbootProject18ApplicationTests {
+class EazySchoolApplicationTests {
 
 	@Test
 	void contextLoads() {
