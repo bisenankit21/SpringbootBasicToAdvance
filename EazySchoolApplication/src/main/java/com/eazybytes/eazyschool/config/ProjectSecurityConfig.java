c16bd0249e922aa317d8ef754266e4e6ceb509d5
ankitbisen <bisenankit21@gmail.com>
2025-03-02 22:40:44 +0530

    Initial commit

---
+++ EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -0,0 +1,73 @@
+package com.eazybytes.eazyschool.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class ProjectSecurityConfig {
+
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+
+  /*            /home
+                /holidays
+                /contact
+                /saveMsg
+                /courses
+                /about*/
+
+        //Permit all requests inside the web application
+        http.csrf((csrf) -> csrf.disable())
+                .authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/home").permitAll()
+                        .requestMatchers("/dashboard").authenticated()
+                .requestMatchers("/holidays/**").permitAll()
+                .requestMatchers("/contact").permitAll()
+                .requestMatchers("/saveMsg").permitAll()
+                .requestMatchers("/courses").permitAll()
+                .requestMatchers("/about").permitAll()
+                .requestMatchers("/assets/**").permitAll()
+                        .requestMatchers("/login").permitAll())
+                .formLogin((form) -> form.loginPage("/login")
+                        .defaultSuccessUrl("/dashboard")
+                        .failureUrl("/login?error=true")
+                        .permitAll())
+                .logout((logout) -> logout.logoutSuccessUrl("/login?logout=true")
+                        .invalidateHttpSession(true).permitAll())
+                .httpBasic(withDefaults());
+        //http.formLogin().loginPage("/login").loginProcessingUrl("/login");
+        //http.httpBasic();
+
+
+        //Deny all requests inside the web application
+//        http.authorizeHttpRequests((requests) -> requests.anyRequest().permitAll());
+//        http.formLogin(withDefaults());
+//        http.httpBasic(withDefaults());
+        return http.build();
+    }
+
+    // if we want to store the users in In-memory of the application
+    @Bean
+    public InMemoryUserDetailsManager userDealsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("12345")
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withDefaultPasswordEncoder()
+                .username("admin")
+                .password("54321")
+                .roles("USER","ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(user,admin);
+    }
+}
