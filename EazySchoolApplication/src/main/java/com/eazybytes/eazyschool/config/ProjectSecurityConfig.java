267b838aa68ea008fa3af2454084a468f2858a1f
ankitbisen <bisenankit21@gmail.com>
2025-03-05 23:08:05 +0530

    Added logic for exception handler.

--- EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.eazybytes.eazyschool.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
