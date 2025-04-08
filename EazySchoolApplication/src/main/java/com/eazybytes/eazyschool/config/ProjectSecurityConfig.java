48c3788280cf3d746190c974d56689be60e4ebf8
ankitbisen <bisenankit21@gmail.com>
2025-04-08 16:45:27 +0530

    Added logic for Password encoder using hashing technique.

--- EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
+++ EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/config/ProjectSecurityConfig.java
@@ -7,6 +7,8 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -43,21 +45,8 @@ public class ProjectSecurityConfig {
         return http.build();
 
     }
-
     @Bean
-    public InMemoryUserDetailsManager userDetailsService() {
-
-        UserDetails user = User.withDefaultPasswordEncoder()
-                .username("user")
-                .password("12345")
-                .roles("USER")
-                .build();
-        UserDetails admin = User.withDefaultPasswordEncoder()
-                .username("admin")
-                .password("54321")
-                .roles("ADMIN")
-                .build();
-        return new InMemoryUserDetailsManager(user, admin);
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
-
 }
--- EazySchoolApplication/src/main/resources/eazyschool_data.sql
+++ EazySchoolApplication/src/main/resources/eazyschool_data.sql
@@ -27,3 +27,8 @@ INSERT INTO `holidays` (`day`,`reason`,`type`,`created_at`, `created_by`)
 
   INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
     VALUES ('STUDENT',CURDATE(),'DBA');
+
+  DELETE FROM person where email='admin@eazyschool.com';
+
+  INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
+    VALUES ('Admin','admin@eazyschool.com','3443434343','$2a$12$EDOVwJqzk3UK/JGldcdQYuOfNNaKkfPUnMxuVAWsvru7H.E8yA2vG', 1 ,CURDATE(),'DBA');
