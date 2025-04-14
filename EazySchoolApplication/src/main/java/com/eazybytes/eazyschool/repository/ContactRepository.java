458b5589669f3e6c6408ab457c3486699fba6f22
ankitbisen <bisenankit21@gmail.com>
2025-04-14 23:15:55 +0530

    Added logic for Pagination concept.

--- EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
+++ EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/repository/ContactRepository.java
@@ -7,10 +7,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -27,7 +31,33 @@ DB related operations and
 public interface ContactRepository extends JpaRepository<Contact,Integer> {
 
     List<Contact> findByStatus(String status);
-    Page<Contact> findByStatus(String status, Pageable pageable);
+
+    //@Query("SELECT c FROM Contact c WHERE c.status = :status")
+    //or we can write like this @Query("SELECT c FROM Contact c WHERE c.status = ?1")
+    @Query(value = "SELECT * FROM contact_msg c WHERE c.status = ?1", nativeQuery = true)
+    Page<Contact> findByStatus(@Param("status") String status, Pageable pageable);
+
+
+    @Transactional
+    @Modifying
+    @Query("UPDATE Contact c SET c.status = ?1 WHERE c.contactId = ?2")
+    int updateStatusById(String status, int id);
+
+    Page<Contact> findOpenMsgs(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    int updateMsgStatus(String status, int id);
+
+    @Query(nativeQuery = true)
+    Page<Contact> findOpenMsgsNative(@Param("status") String status, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(nativeQuery = true)
+    int updateMsgStatusNative(String status, int id);
+
+
 
    /* private final JdbcTemplate jdbcTemplate;
 
