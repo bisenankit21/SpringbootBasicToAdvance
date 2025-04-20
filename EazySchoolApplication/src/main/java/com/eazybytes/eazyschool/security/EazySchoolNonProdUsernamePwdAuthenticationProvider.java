778e7a72c2a10cfd06c684a470c37d4110702182
ankitbisen <bisenankit21@gmail.com>
2025-04-20 13:30:50 +0530

    Added logic related profiles. prod and non-prod.

--- EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/security/EazySchoolNonProdUsernamePwdAuthenticationProvider.java
+++ EazySchoolApplication/src/main/java/com/eazybytes/eazyschool/security/EazySchoolNonProdUsernamePwdAuthenticationProvider.java
@@ -19,10 +19,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Profile("prod")
-public class EazySchoolUsernamePwdAuthenticationProvider
-        implements AuthenticationProvider
-{
+@Profile("!prod")
+public class EazySchoolNonProdUsernamePwdAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private PersonRepository personRepository;
 
@@ -35,8 +33,7 @@ public class EazySchoolUsernamePwdAuthenticationProvider
         String email = authentication.getName();
         String pwd = authentication.getCredentials().toString();
         Person person = personRepository.readByEmail(email);
-        if(null != person && person.getPersonId()>0 &&
-                passwordEncoder.matches(pwd, person.getPwd())){
+        if(null != person && person.getPersonId()>0){
             return new UsernamePasswordAuthenticationToken(
                     email, null, getGrantedAuthorities(person.getRoles()));
         }else{
